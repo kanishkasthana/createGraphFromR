83012f3a03f70a6e288a96f884ac1ff6a7682ec0
Kanishk Asthana <kani.asthana@gmail.com>
2015-10-30 16:31:56 -0700

    Adding code for nodes and edges to be created for the graph

--- src/creategraphfromr/edge.java
+++ src/creategraphfromr/edge.java
@@ -13,5 +13,32 @@ import java.util.*;
  * @author kanis_000
  */
 public class edge {
+    public static List <edge> alledges=new <edge>ArrayList();
+    node parent;
+    node child;
+    double weight;
+    
+    public edge(node parent,node child, double weight){
+        this.parent=parent;
+        this.child=child;
+        this.weight=weight;
+        this.parent.addChild(child);
+        this.child.addParent(parent);
+        this.parent.addEdge(this);
+        this.child.addEdge(this);
+        this.alledges.add(this);
+    }
+    
+    public node getParent(){
+        return parent;
+    }
+    
+    public node getChild(){
+        return child;
+    }
+    
+    public double getWeight(){
+        return weight;
+    }
     
 }
--- src/creategraphfromr/node.java
+++ src/creategraphfromr/node.java
@@ -13,5 +13,43 @@ import java.util.*;
  * @author kanis_000
  */
 public class node {
+    public static List<node> allnodes=new <node>ArrayList();
+    String nodeName;
+    List <edge>edges=new <edge>ArrayList();
+    List <node>parents=new <node>ArrayList();
+    List <node>children=new <node>ArrayList();
+    
+    public node(String value){
+        this.nodeName=value;
+        allnodes.add(this);
+    }
+    
+    public void addChild(node child){
+        children.add(child);
+    }
+    
+    public void addParent(node parent){
+        parents.add(parent);
+    }
+    
+    public List<node> getChildren(){
+        return this.children;
+    }
+    
+    public List<node> getParents(){
+        return this.parents;
+    }
+    
+    public void addEdge(edge newedge){
+        this.edges.add(newedge);
+    }
+    
+    public List<edge> getEdges(){
+        return edges;
+    }
+    
+    public String getNodeName(){
+        return nodeName;
+    }
     
 }
